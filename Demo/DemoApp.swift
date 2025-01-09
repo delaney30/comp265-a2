f80ce5136a0dd7426aa50644cca0060aad4bc722
Delaney Lehouillier <dlehouillier@hotmail.ca>
2025-01-09 09:29:22 -0600

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Delaney Lehouillier on 2025-01-09.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoTests/DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTests.swift
+//  DemoTests
+//
+//  Created by Delaney Lehouillier on 2025-01-09.
+//
+
+import Testing
+@testable import Demo
+
+struct DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
